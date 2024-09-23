12365ec9bfd94d82098b1998bb370944e5b8a3b3
Tomek3254 <tomekkarczewski200@gmail.com>
2024-09-23 09:49:39 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Hero } from './heroes/hero';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'tour-of-heroes';
+}
